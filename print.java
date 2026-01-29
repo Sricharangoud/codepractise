3d7cab360491088e8dc58333cd1adee8203a4606
P. SRICHARAN GOUD <162333339+Sricharangoud@users.noreply.github.com>
2026-01-29 21:32:03 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,36 @@
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.print("Hello World");
+//     }
+// }
+
+
+// import java.io.*;
+
+// class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedWriter bw = new BufferedWriter(
+//                 new OutputStreamWriter(System.out)
+//         );
+//         bw.write("Hello World");
+//         bw.flush();
+//     }
+// }
+
+
+// import java.io.*;
+
+// class Main {
+//     public static void main(String[] args) {
+//         PrintWriter pw = new PrintWriter(System.out);
+//         pw.print("Hello World");
+//         pw.flush();
+//     }
+// }
+
+
+// class Main {
+//     public static void main(String[] args) {
+//         System.err.print("Hello World");
+//     }
+// }
