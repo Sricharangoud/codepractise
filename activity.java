b0c53c69e216a00939af1da5baa153c56f73bf37
P. SRICHARAN GOUD <162333339+Sricharangoud@users.noreply.github.com>
2026-01-30 21:28:43 +0530

    Add files via upload

---
+++ activity.java
@@ -0,0 +1,45 @@
+public class activity {
+    static void morning() {
+        System.out.println("I woke up in the morning.");
+        System.out.println("I used my smartphone for some time.");
+    }
+    static void doExercise() {
+        System.out.println("I did some exercise.");
+    }
+    static void haveBreakfast() {
+        System.out.println("I took my breakfast.");
+    }
+    static void college() {
+        System.out.println("I get ready for college.");
+        System.out.println("I went to Training and attended the Java Training.");
+    }
+    static void Training() {
+        System.out.println("I have learn Java Programming");
+    }
+    static void lunch() {
+        System.out.println("I had my lunch");
+    }
+    static void Evening() {
+        System.out.println("I was went to Hostel");
+        System.out.println("I was spend some to learn java");
+    }
+    static void dinner() {
+        System.out.println("I had my dinner");
+    }
+    static void sleep() {
+        System.out.println("I was slept by 11pm");
+    }
+    public static void main(String[] a) {
+        System.out.println("My Today's Activities");
+        morning();
+        doExercise();
+        haveBreakfast();
+        college();
+        Training();
+        lunch();
+        Evening();
+        dinner();
+        sleep();
+        System.out.println("End of Day");
+    }
+}
