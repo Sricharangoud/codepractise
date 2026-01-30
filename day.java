a0117a7d430c488373461d9f607c5c428ead97db
P. SRICHARAN GOUD <162333339+Sricharangoud@users.noreply.github.com>
2026-01-30 21:30:05 +0530

    Add files via upload

---
+++ day.java
@@ -0,0 +1,18 @@
+public class day {
+    public static void main(String[] args) {
+        int day = 4;
+
+        String result = switch (day) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "Invalid day";
+        };
+
+        System.out.println(result);
+    }
+}
